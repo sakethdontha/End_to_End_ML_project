98186b8fa208d471bb85f288a4b8f85ef739a3e0
saketh <sakethdontha2001@gmail.com>
2025-01-08 22:18:44 -0600

    model evaluation added

--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -3,7 +3,7 @@ from mlproject import logger
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from mlproject.entity.config_entity import DataTransformationConfig
-
+from sklearn.preprocessing import MinMaxScaler
 
 class DataTransformation:
     def __init__(self, config: DataTransformationConfig):
@@ -16,12 +16,26 @@ class DataTransformation:
 
     # I am only adding train_test_spliting cz this data is already cleaned up
 
+    def dropcolumn(self):
+        data=pd.read_csv(self.config.data_path)
+        data=data.drop("Serial No.", axis=1)
+
+        return data
+
+    def MinMaxScaler(self):
+        data = self.dropcolumn()
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        data["GRE Score"] = scaler.fit_transform(data[["GRE Score"]])
+        data["TOEFL Score"] = scaler.fit_transform(data[["TOEFL Score"]])
+
+        return data
+
 
     def train_test_spliting(self):
-        data = pd.read_csv(self.config.data_path)
+        data = self.MinMaxScaler()
 
         # Split the data into training and test sets. (0.75, 0.25) split.
-        train, test = train_test_split(data)
+        train, test = train_test_split(data,test_size=0.2,random_state=42)
 
         train.to_csv(os.path.join(self.config.root_dir, "train.csv"),index = False)
         test.to_csv(os.path.join(self.config.root_dir, "test.csv"),index = False)
--- src/mlproject/entity/config_entity.py
+++ src/mlproject/entity/config_entity.py
@@ -33,3 +33,12 @@ class ModelTrainerConfig:
     C: float
     epsilon: float
     target_column: str
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    test_data_path: Path
+    model_path: Path
+    all_params: dict
+    metric_file_name: Path
+    target_column: str
